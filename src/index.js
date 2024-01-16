627a98203a21fa80b34a2758c1ef1e2fee78c67e
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-01-16 13:15:22 +0545

    created chip-component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter path="/chip-input-component">
+    <BrowserRouter basename="/chip-input-component">
       <App />
     </BrowserRouter>
   </React.StrictMode>
